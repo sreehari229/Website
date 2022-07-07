3fdabf5d1a2faa0ea43bf8f7f4c37f7ed49cca13
sreehari <sreehari229@gmail.com>
2022-07-07 13:11:26 +0530

    about section modified

--- CurVitae/views.py
+++ CurVitae/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'CurVitae/index.html')
+    data = {
+        "name" : "Sreehari S",
+
+        "about" : "Hello! I am Sreehari S. Currently pursuing Masters in Computer Science. Strongly motivated individual with Bachelor's degree in Computer Applications. Interested in exploring fields like ML/AI. Offering effective coding skills and working experience with multiple projects.",
+
+        "about_section" : {
+            'age' : '22',
+            'email' : "sreehari229@gmail.com",
+            'phone' : "+91 - 8971 422 625",
+            'address' : "Bangalore, India",
+        },
+
+        "professional_skills_left" : {
+            "Python" : 90,
+            "C#" : 85,
+            "Java" : 85,
+            "Microsoft Sql Server" : 70,
+        },
+
+        "professional_skills_right" : {
+            "Flask / Django" : 70,
+            "ASP.net MVB" : 85,
+            "MySQL" : 80,
+            "Git\Github" : 90,
+        },
+              
+    }
+    return render(request, 'CurVitae/index.html', data)
--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zq5vad8dwnjs_9umlqeba_#wy6ab=)4u^k&o3zsdmy+-e5^#6!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
